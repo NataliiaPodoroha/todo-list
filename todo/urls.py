4a46ac8bee0e05b26961bc74943cb09175a0e361
Nataliia Podoroha <ingenuous999@gmail.com>
2023-08-18 12:02:05 +0200

    Add CRUD for Task model

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,35 @@
 from django.urls import path
 
+from todo.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    ToggleTaskStatusView,
+)
+
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path(
+        "/create/",
+        TaskCreateView.as_view(),
+        name="task-create",
+    ),
+    path(
+        "/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update",
+    ),
+    path(
+        "/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete",
+    ),
+    path(
+        "/<int:pk>/toggle-status/",
+        ToggleTaskStatusView.as_view(),
+        name="toggle-task-status",
+    ),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from todo.forms import TaskForm
+from todo.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.all()
+    context_object_name = "task_list"
+    template_name = "todo/index.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+class ToggleTaskStatusView(generic.View):
+    def post(self, request, pk):
+        task = get_object_or_404(Task, id=pk)
+
+        task.is_completed = False if task.is_completed else True
+        task.save()
+
+        return HttpResponseRedirect(
+            reverse_lazy("todo:task-list")
+        )
