f03ca1b33eff67d27cfc81c2325f79ca3440f2ad
Nataliia Podoroha <ingenuous999@gmail.com>
2023-08-18 11:53:04 +0200

    Add forms.py and create forms dor models.

---
+++ todo/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+from todo.models import Task, Tag
+
+
+class TaskForm(forms.ModelForm):
+    tag = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    deadline = forms.DateField(
+        required=False,
+        widget=forms.DateTimeInput(attrs={'type': 'date'}),
+    )
+
+    class Meta:
+        model = Task
+        fields = [
+            "content",
+            "deadline",
+            "tag",
+            "is_completed"
+        ]
+
+
+class TagForm(forms.ModelForm):
+
+    class Meta:
+        model = Tag
+        fields = "__all__"
